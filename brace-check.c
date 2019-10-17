edbb12956b445c5a72db4e5e03bde120eac323d5
ConnorGuidry <connor.guidry@gmail.com>
2019-10-17 12:16:47 -0500

    added commenting

--- brace-check.c
+++ brace-check.c
@@ -1,11 +1,20 @@
+/* The idea for this program was taken from an exercise in The C Programming
+Language, 2nd ed., by K&R. This is purely for educational purposes and will
+not see serious development beyond the scope of the exercise
+
+This program checks a test file for basic C syntax errors with respect to braces
+of all types, including comments. */
+
 #include <stdio.h>
 #include <regex.h>
 
 int main(int argc, char *argv[])
 {
+	//Pointer to a FILE type
 	FILE *fp;
 	char ch;
 
+	//Opens file from argument in READ mode, fp points to file
 	fp = fopen(argv[1], "r");
 	if (fp == NULL)
 	{
@@ -13,11 +22,13 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
+	//While current char is not EOF
 	while ((ch = getc(fp)) != EOF)
 	{
 		printf("%c", ch);	
 	}
 	
+	//Closes file
 	fclose(fp);
 
 	return 0;
