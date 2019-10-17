ddb240633a267163195b7df16be4b01f5cac6645
ConnorGuidry <connor.guidry@gmail.com>
2019-10-17 11:58:36 -0500

    began implementing fileio using fopen and getc

---
+++ brace-check.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <regex.h>
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	char ch;
+
+	fp = fopen(argv[1], "r");
+	if (fp == NULL)
+	{
+		printf("Error opening selected file.\n");
+		return 1;
+	{
+
+	while ((ch = getc(fp)) != EOF)
+	{
+		printf("%c", ch);	
+	}
+	
+	fclose(fp);
+
+	return 0;
+}
